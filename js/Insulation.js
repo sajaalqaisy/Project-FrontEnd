2d8ccf40d3313abdf011a1cf14fe9dfcd7f7ceb7
sajaalqaisy <osaja8304@gmail.com>
2025-02-13 21:00:01 +0300

    Update Insulation.js

--- js/Insulation.js
+++ js/Insulation.js
@@ -3,11 +3,5 @@ function ourproduct(){
     window.location.href="./ourProduct.html"
 }
 
-var cssfile=document.getElementById('cssfile')
-cssfile.href='css/ourProduct.css'
-    if (cssfile.href=='black'){
-      document.body.style.backgroundColor='black'
-    }
-else{
-    document.body.style.backgroundColor='white' 
-}
+
+
